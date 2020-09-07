756b2424281bfb6113629e3a0fe021d6ec3465a3
Vladimir Tsallagov <tsallagov.vladimir@bk.ru>
2020-09-07 11:22:42 +0300

    empty

--- API/API/products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Clothes)
-admin.site.register(Book)
-admin.site.register(TV)
-admin.site.register(Consoles)
-admin.site.register(Audio)
-admin.site.register(Garden)
